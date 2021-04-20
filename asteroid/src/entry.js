6b4c702a8102096f6ae3abb49c98ff19d58a1a85
Samantha Lilly <samanthalillydesign@gmail.com>
2021-04-20 11:19:22 -0400

    Set up filetree

---
+++ asteroid/src/entry.js
@@ -0,0 +1,8 @@
+require("./asteroid.js");
+require("./bullet.js");
+require("./entry.js");
+require("./game_view.js");
+require("./game.js");
+require("./moving_object.js");
+require("./ship.js");
+require("./utils.js");
